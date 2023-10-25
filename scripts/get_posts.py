f6f2b2d9c2f660d27b060360d20e07585c9b5fda
Adam Ksiezyk <adam.ksiezyk@gmail.com>
2023-10-25 20:25:57 +0200

    Refactor posts fetching script

--- scripts/get_posts.py
+++ scripts/get_posts.py
@@ -4,15 +4,20 @@ import datetime as dt
 import logging
 import os
 from dataclasses import asdict
-from typing import Callable, Coroutine, Optional
+from functools import partial
+from typing import Callable, Coroutine
 
+import httpx
 import yaml
-from bs4 import BeautifulSoup
+from bs4 import PageElement
 from pocketbase import PocketBase
 from pocketbase.client import ClientResponseError, RecordService
+from pocketbase.models.record import BaseModel
 
 from social_network.facebook_posts import create_post_from_soup, fetch_feed_generator, get_timestamp
 from social_network.facebook_scraper import create_session
+
+TaskMessage = tuple[str, PageElement]
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -22,69 +27,51 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 CONFIG_PATH = './config.yml'
+NUM_WORKERS = 4
 
 
-async def fetch_page_posts(page_id: str, today: dt.date):
-    logger.info(f"Starting scraping posts for {page_id!r} {today}")
-
-    fb_email = os.environ["FB_EMAIL"]
-    fb_password = os.environ["FB_PASSWORD"]
-    pb_host = os.environ["PB_HOST"]
-    pb_email = os.environ["PB_EMAIL"]
-    pb_password = os.environ["PB_PASSWORD"]
-
-    num_workers = 4
-    queue = asyncio.Queue()
-
-    client = PocketBase(pb_host)
-    client.admins.auth_with_password(pb_email, pb_password)
-    pages_collection = client.collection('pages')
-    posts_collection = client.collection('posts')
-
+def get_or_create_page(pages_collection: RecordService, page_name: str) -> BaseModel:
     try:
-        page = pages_collection.get_first_list_item(f'name = "{page_id}"')
+        return pages_collection.get_first_list_item(f'name = "{page_name}"')
     except ClientResponseError:
-        logger.warning(f'Page {page_id!r} does not exist, creating new entry')
-        page = pages_collection.create({'name': page_id})
-
-    async with create_session(fb_email, fb_password) as s:
-        async def fetch_post(post_soup: BeautifulSoup) -> Optional[dict]:
-            res = await create_post_from_soup(s, post_soup)
-            if res is None:
-                return None
-            res_dict = asdict(res)
-            return {'page': page.id, 'post': res_dict}
-        posts_soups = fetch_feed_generator(s, page_id)
-
-        async for post_soup in posts_soups:
-            if get_timestamp(post_soup).date() < today:
-                break
-            await queue.put(post_soup)
-        logger.info(f"Found {queue.qsize()} posts to fetch")
-        logger.info(f"Creating {num_workers} workers")
-        workers = [asyncio.create_task(worker(queue, posts_collection, fetch_post)) for _ in range(num_workers)]
-        await queue.join()
-
-        logger.info("Finished processing tasks queue, cancelling workers")
-        for w in workers:
-            w.cancel()
+        logger.warning(f'Page {page_name!r} does not exist, creating new entry')
+        return pages_collection.create({'name': page_name})
 
 
-async def worker(input: asyncio.Queue[BeautifulSoup],
-                 pb: RecordService,
-                 f: Callable[[BeautifulSoup], Coroutine[None, None, Optional[dict]]]) -> None:
+async def fetch_page_posts(s: httpx.AsyncClient,
+                           queue: asyncio.Queue[TaskMessage],
+                           page: BaseModel,
+                           today: dt.date) -> None:
+    logger.info(f"Starting scraping posts for {page.name!r} {today}")
+    posts_soups = fetch_feed_generator(s, page.name)
+
+    i = 0
+    async for post_soup in posts_soups:
+        if get_timestamp(post_soup).date() < today:
+            break
+        await queue.put((page.id, post_soup))
+        i += 1
+    logger.info(f"Found {i} posts to fetch")
+
+
+async def fetch_post(s: httpx.AsyncClient, collection: RecordService, task_message: TaskMessage) -> None:
+    page_id, post_soup = task_message
+    res = await create_post_from_soup(s, post_soup)
+    if res is None:
+        logger.warning("Could not fetch post")
+        return
+    logger.debug("Uploading post")
+    collection.create({'page': page_id, 'post': asdict(res)})
+
+
+async def worker(input: asyncio.Queue[TaskMessage], f: Callable[[TaskMessage], Coroutine]) -> None:
     logger.debug("Worker created, waiting for tasks")
     while True:
         arg = await input.get()
         logger.debug("Task received, processing")
         try:
             # Process task
-            ret = await f(arg)
-            if ret is None:
-                logger.warning(f"Function {f.__name__} returned None")
-            else:
-                logger.debug(f"Task {f.__name__} completed successfuly")
-                pb.create(ret)
+            await f(arg)
         except asyncio.CancelledError:
             # Worker cancelled, exit
             logger.debug("Worker cancelled")
@@ -102,8 +89,33 @@ async def main() -> None:
         config = yaml.safe_load(f)
     pages = config['pages']
     today = dt.date.today()
-    for page_id in pages:
-        await fetch_page_posts(page_id, today)
+
+    pb_host = os.environ["PB_HOST"]
+    pb_email = os.environ["PB_EMAIL"]
+    pb_password = os.environ["PB_PASSWORD"]
+    logger.info(f"Connecting to PocketBase {pb_host}")
+    client = PocketBase(pb_host)
+    client.admins.auth_with_password(pb_email, pb_password)
+    pages_collection = client.collection('pages')
+    posts_collection = client.collection('posts')
+
+    logger.info(f"Creating {NUM_WORKERS} workers")
+    queue = asyncio.Queue()
+
+    fb_email = os.environ["FB_EMAIL"]
+    fb_password = os.environ["FB_PASSWORD"]
+    async with create_session(fb_email, fb_password) as s:
+        f = partial(fetch_post, s, posts_collection)
+        workers = [asyncio.create_task(worker(queue, f)) for _ in range(NUM_WORKERS)]
+
+        for page_name in pages:
+            page = get_or_create_page(pages_collection, page_name)
+            await fetch_page_posts(s, queue, page, today)
+
+    await queue.join()
+    logger.info("Finished processing tasks queue, cancelling workers")
+    for w in workers:
+        w.cancel()
 
 if __name__ == "__main__":
     asyncio.run(main())
